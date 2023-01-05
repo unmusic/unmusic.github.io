6e8d1da493f6cb141fe98d541b6d2a0a6eded579
Sivadass N <nsivadass@gmail.com>
2023-01-05 23:16:39 +0530

    Added official website link

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -11,13 +11,24 @@ const Header = () => {
       <Link className="logo" to="/" onClick={() => handleClick()}>
         <img src={ImgUnMusicLogo} alt="UnMusic" />
       </Link>
-      <a
-        href="https://github.com/unmusic/unmusic.github.io"
-        target="_blank"
-        title="Fork it on Github"
-      >
-        <img src="/media-assets/github.svg" alt="Fork it on Github" />
-      </a>
+
+      <p>
+        <span>Try now on </span>
+        <a
+          href="https://unmusic.app"
+          title="Try it now on our official website"
+        >
+          www.unmusic.app
+        </a>
+        <span>&nbsp;</span>
+        <a
+          href="https://github.com/unmusic/unmusic.github.io"
+          target="_blank"
+          title="Fork it on Github"
+        >
+          <img src="/media-assets/github.svg" alt="Fork it on Github" />
+        </a>
+      </p>
     </header>
   );
 };
